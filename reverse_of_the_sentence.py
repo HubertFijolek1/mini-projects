ca7ea78520a70358f92ada8fcf7f474d8618afc4
HubertFijolek1 <hubertfijolek1@gmail.com>
2021-10-19 09:47:23 +0200

    reverse of the sentence

---
+++ reverse_of_the_sentence.py
@@ -0,0 +1,13 @@
+sentence = "skaerf nohtyp lla ot olleh"
+# correct_sentence = []
+# for i in sentence.split(" "):
+#     correct_sentence.append(i[::-1])
+# correct_sentence.reverse()
+# correct_sentence=" ".join(correct_sentence)
+
+# print(correct_sentence)
+
+list1 = []
+correct_sentence=[i[::-1] for i in sentence.split(" ") ]
+correct_sentence.reverse()
+print(correct_sentence)
