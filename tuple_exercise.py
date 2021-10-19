e2ce2198519d83362ed2051be6b279edad6c706a
HubertFijolek1 <hubertfijolek1@gmail.com>
2021-10-19 09:46:32 +0200

    Exercise of tuple

---
+++ tuple_exercise.py
@@ -0,0 +1,15 @@
+#we check if all elements in a given tuple are in the other tuple
+
+tuple1 = (3,4,5)
+tuple2 = (1,2,3,4,5,6,7,8,9)
+
+def check_tuples(tuple1, tuple2):
+    for element in tuple1:
+        if element not in tuple2:
+            return False
+    return True
+
+print(check_tuples(tuple1, tuple2))
+
+tuple3 = (5,3,2,456)
+print(check_tuples(tuple3, tuple2))
